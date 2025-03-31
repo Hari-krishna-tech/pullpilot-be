7c8c7ce1474333210ed12f85bb971223ebfe94e0
Hari-krishna-tech <jvenkadam@gmail.com>
2025-03-31 10:31:21 +0530

    initilized project

---
+++ src/test/java/com/github/hari_krishna_tech/pullpilot/PullpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hari_krishna_tech.pullpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
